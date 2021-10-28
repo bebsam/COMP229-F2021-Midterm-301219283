e4d3f5c9167389e2308f1330e8b43a46a4c477af
Bebin Samuel <bobsemul@gmail.com>
2021-10-28 18:33:12 -0400

    DB switch

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "URI": "mongodb://localhost/books229" //local MongoDB deployment
-  //"URI": "mongodb+srv://Admin:aM6j1EHVnapL5jhM@midtermdb.upefn.mongodb.net/book_list?retryWrites=true&w=majority"
+  //"URI": "mongodb://localhost/books229" //local MongoDB deployment
+  "URI": "mongodb+srv://Admin:aM6j1EHVnapL5jhM@midtermdb.upefn.mongodb.net/book_list?retryWrites=true&w=majority"
 };
