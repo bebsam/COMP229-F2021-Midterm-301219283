95728dddafadf3fcb3fe27434fc25e93e4269db8
Bebin Samuel <bobsemul@gmail.com>
2021-10-28 20:09:46 -0400

    Final clenup

--- server/models/books.js
+++ server/models/books.js
@@ -8,7 +8,6 @@ let mongoose = require('mongoose');
 // create a model class
 let Book = mongoose.Schema({
     Title: String,
-    Description: String,
     Price: Number,
     Author: String,
     Genre: String
